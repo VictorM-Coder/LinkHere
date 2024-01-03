3cd04a4d246e78ff68b3746d6c1d46f6077e7530
VictorM-Coder <martinsvictor6781@gmail.com>
2024-01-03 10:23:15 -0300

    feat: configure project bases

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        'singleQuote': true,
+        'semi': false,
+        'singleAttributePerLine': true
+      }
+    ]
+  }
+}

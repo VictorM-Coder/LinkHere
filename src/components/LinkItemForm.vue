a6e86a6d37c92c5d174bf8ea4a2d38feb5b50a0d
VictorM-Coder <martinsvictor6781@gmail.com>
2024-02-14 17:10:42 -0300

    feat: validate editInputText component

--- src/components/LinkItemForm.vue
+++ src/components/LinkItemForm.vue
@@ -6,7 +6,32 @@ import DeleteIcon from '@/components/icons/DeleteIcon.vue'
 import CopyContent from '@/components/icons/CopyContent.vue'
 import EditInputText from '@/components/form/EditInputText.vue'
 import ClipboardUtil from '@/utils/ClipboardUtil'
-import { useToast } from "primevue/usetoast"
+import { useToast } from 'primevue/usetoast'
+import { onMounted, ref } from 'vue'
+import * as yup from 'yup'
+import { useForm } from 'vee-validate'
+
+const editTitle = ref<Boolean>(false)
+const editLink = ref<Boolean>(false)
+
+const schema = yup.object({
+  link: yup
+    .string()
+    .matches(
+      /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+      'Enter a correct url!',
+    )
+    .required('Please enter a link')
+    .label('link'),
+  title: yup.string().required().label('title'),
+})
+
+const { defineField, handleSubmit, errors } = useForm({
+  validationSchema: schema,
+})
+
+const [title] = defineField('title')
+const [link] = defineField('link')
 
 const toast = useToast()
 const props = defineProps({
@@ -16,6 +41,11 @@ const props = defineProps({
   },
 })
 
+onMounted(() => {
+  title.value = props.linkItem?.title
+  link.value = props.linkItem?.link
+})
+
 const emit = defineEmits(['onUpdate'])
 
 function updateTitle(title: string) {
@@ -23,6 +53,7 @@ function updateTitle(title: string) {
     title: title,
     link: props.linkItem.link,
   })
+  editTitle.value = false
 }
 
 function updateLink(link: string) {
@@ -30,6 +61,7 @@ function updateLink(link: string) {
     title: props.linkItem.title,
     link: link,
   })
+  editLink.value = false
 }
 
 function copyLink() {
@@ -44,21 +76,37 @@ function showSuccess() {
     life: 3000,
   })
 }
+
+function onEditTitle() {
+  editTitle.value = true
+}
+
+function onEditLink() {
+  editLink.value = true
+}
 </script>
 
 <template>
-  <li class="flex w-full items-center rounded-2xl bg-slate-800 px-6 py-8">
+  <li class="flex w-full items-center rounded-2xl bg-slate-800 p-6">
     <div class="me-6 flex">
       <drag-icon class="handle text-pink-200 opacity-80 hover:cursor-grab" />
     </div>
-    <div class="w-full">
+    <form class="w-full">
       <edit-input-text
-        :value="linkItem.title"
+        name="title"
+        :value="title"
+        type="title"
+        :editable="editTitle"
         @on-update="updateTitle"
+        @on-edit="onEditTitle"
       />
       <edit-input-text
-        :value="linkItem.link"
+        name="link"
+        :value="link"
+        :editable="editLink"
+        type="link"
         @on-update="updateLink"
+        @on-edit="onEditLink"
       />
       <div class="flex items-center justify-end">
         <icon-button
@@ -72,6 +120,6 @@ function showSuccess() {
           <delete-icon />
         </icon-button>
       </div>
-    </div>
+    </form>
   </li>
 </template>
--- src/components/form/EditInputText.vue
+++ src/components/form/EditInputText.vue
@@ -3,43 +3,67 @@ import InputGroup from 'primevue/inputgroup'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
 import EditIcon from '@/components/icons/EditIcon.vue'
-import { ref } from 'vue'
+import { toRef } from 'vue'
+import { useField } from 'vee-validate'
 
 const props = defineProps({
   value: {
     type: String,
     required: true,
   },
+  type: {
+    type: String,
+    required: true,
+  },
+  editable: {
+    type: Boolean,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
 })
 
-const emit = defineEmits(['onUpdate'])
-const edit = ref<Boolean>(false)
-let valueInput: string
+const name = toRef(props, 'name')
 
-function changeFocus() {
-  edit.value = false
-  if (props.value != valueInput) {
-    emit('onUpdate', valueInput)
-  }
+const { value: inputValue, errorMessage } = useField(name, undefined, {
+  initialValue: props.value,
+})
+
+const emit = defineEmits(['onUpdate', 'failUpdate', 'onEdit'])
+
+function handleUpdate() {
+  if (!errorMessage.value) emit('onUpdate', inputValue.value)
 }
 </script>
 
 <template>
-  <InputGroup class="mb-6">
-    <InputText
-      id="input-title"
-      v-model="valueInput"
-      :value="value"
-      class="w-full p-2.5"
-      type="text"
-      :disabled="!edit"
-      @focusout="changeFocus"
-    />
-    <Button
-      class="bg-slate-900 px-4"
-      @click="edit = true"
+  <div class="field my-6">
+    <InputGroup>
+      <InputText
+        :id="name"
+        v-model="inputValue"
+        :name="name"
+        :value="inputValue"
+        class="w-full p-2.5"
+        type="text"
+        :disabled="!editable"
+        aria-describedby="title-help"
+        :class="{ 'p-invalid': errorMessage }"
+        @focusout="handleUpdate"
+      />
+      <Button
+        class="bg-slate-900 px-4"
+        @click="emit('onEdit')"
+      >
+        <edit-icon />
+      </Button>
+    </InputGroup>
+    <small
+      id="title-help"
+      class="p-error"
+      >{{ errorMessage }}</small
     >
-      <edit-icon />
-    </Button>
-  </InputGroup>
+  </div>
 </template>
